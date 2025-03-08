e1c76ccedccb18960522387be644f5ed4254ae46
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-03-08 16:11:29 -0300

    .

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -1,5 +1,5 @@
 import {WhatsappLogo, InstagramLogo} from '@phosphor-icons/react/dist/ssr'
-import logoImg from '../../../public/logo transparente.png'
+import logoImg from '../../../public/new logo transparente.png'
 import Image from 'next/image'
 
 
