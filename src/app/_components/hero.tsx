3cf4e8c096cff27b5b8b9b50279ef5af3b8ccb97
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-03-08 16:59:33 -0300

    .

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -12,7 +12,7 @@ export function Hero() {
                 src={bg}
                 alt='logo'
                 fill
-                sizes='(max-width: 100%)'
+                sizes='(max-width: 70%)'
             />
 
             <div className='container mx-auto pt-16 pb-16 md:pb-0 px-4 relative'>
