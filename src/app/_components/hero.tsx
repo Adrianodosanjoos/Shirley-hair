168ff414df2822626cf42793b443e8fa13cc7195
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-03-08 13:58:45 -0300

    atualixação

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -1,6 +1,24 @@
 export function Hero() {
     return(
-        <section className="bg-amber-950 text-white">
+        <section className="bg-amber-950 text-white relative overflow-hidden">
+
+            <div>
+
+              <article>
+
+                   <div>
+                    <h1>
+                        Shirlei Nátali 
+
+                        BraidsDesigner
+
+                        Tranças todos os tipos de penteados p/noivas Entrelaçamento/Nó Italiano Colagem e restauração de laces e wigs Cursos e mentorias vendas de produtos e acessórios p/cabelo.
+                    </h1>
+                   </div>
+
+              </article>
+
+            </div>
            
         </section>
     )
