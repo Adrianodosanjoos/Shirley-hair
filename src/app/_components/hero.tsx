acaf1e30a22aa1fd06bef0a221e0f87fbcd6791c
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-03-08 17:18:44 -0300

    .

--- src/app/_components/hero.tsx
+++ src/app/_components/hero.tsx
@@ -12,7 +12,7 @@ export function Hero() {
                 src={bg}
                 alt='logo'
                 className='hidden md:block h-full relative'
-                sizes='(max-width: 768px) 0 px, 50vw'
+                sizes='(max-width: 50%) 0 px, 50vw'
                 
             />
 
