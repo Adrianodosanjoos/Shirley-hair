94f28d693a88fc7a5096f515e481c8afb3d53a95
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-03-09 11:27:50 -0300

    .

--- src/app/_components/map.tsx
+++ src/app/_components/map.tsx
@@ -1,7 +1,25 @@
+import Image from "next/image"
+import fotoInfantil1 from '../../../public/trança infantil6.jpg'
+import fotoInfantil2 from '../../../public/trança infantil5.jpg'
 export function Map () {
     return (
-        <div>
-            <h1>Mapa</h1>
-        </div>
+        <section className="bg-black py-16">
+            <div className="container mx-auto grid-cols-1 lg:grid-cols-2 gap-12
+            items-center">
+
+                <div className="relative w-full h-[400px] rounded-3xl overflow-hidden">
+                    <Image
+                    src={fotoInfantil1}
+                    alt="fotoInfantil1"
+                    fill
+                    quality={100}
+                    className="object-cover hover:scale-110 duration-300"
+                    priority
+                    />
+                </div>
+
+            </div>
+
+        </section>
     )
 }
