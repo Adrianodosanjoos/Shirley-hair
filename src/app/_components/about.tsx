1a54b5d92c8f3aabd813dcfa973c440d59d356a9
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-03-10 06:40:47 -0300

    .

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -46,7 +46,7 @@ export function About() {
                     alt="imgfundo"
                     fill
                     priority
-                    className='object-cover opacity-50 lg:max-h-12 mt-14'
+                    className='object-cover opacity-50 lg:max-h-5 mt-64'
 
                 />
 
