0a2d1f0146156f4c70569bf65c31eb1f77a938a5
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-03-11 06:45:16 -0300

    .

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -42,14 +42,14 @@ export function About() {
 
                     </div>
 
-                    <div className="space-y-6 text-white relative overflow-hidden mt-10">
+                    <div className="space-y-6 text-white relative overflow-hidden mt-10 lg:flex">
 
                         <Image
                             src={imgfundo}
                             alt="imgfundo"
                             fill
                             priority
-                            className='object-cover opacity-50 lg:max-w-xl lg:ml-117 lg:mt-2'
+                            className='object-cover opacity-50 lg:max-w-xl lg:ml-117 lg:mt-2 lg:items-center lg:content-center'
 
                         />
 
