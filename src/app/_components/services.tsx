e6023e19a85a4cc0b6ee01b4fde39b0f1171a883
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-03-11 13:07:35 -0300

    .

--- src/app/_components/services.tsx
+++ src/app/_components/services.tsx
@@ -3,7 +3,7 @@ export function Services() {
      <section className="bg-black py-16">
         <div className="container mx-auto px-4">
             <div>
-                <h2 className="font-serif font-4xl font-with">Serviços</h2>
+                <h2 className="font-serif text-4xl font-with">Serviços</h2>
             </div>
         </div>
         
