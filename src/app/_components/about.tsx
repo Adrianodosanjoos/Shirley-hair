2084dc3ecbfa7ff10fdfd930985f5a72d7161233
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-03-11 06:33:16 -0300

    .

--- src/app/_components/about.tsx
+++ src/app/_components/about.tsx
@@ -17,13 +17,13 @@ export function About() {
 
                     <div className="relative">
 
-                        <div className="object-cover  w-full h-[400px] rounded-3xl overflow-hidden">
+                        <div className="object-cover mb-20 w-full h-[400px] rounded-3xl overflow-hidden  lg:flex">
                             <Image
                                 src={perfil}
                                 alt="perfil"
                                 fill
                                 quality={100}
-                                className="object-cover hover:scale-110 duration-300"
+                                className="object-cover hover:scale-110 duration-300  lg:items-center lg:content-center"
                                 priority
                             />
                         </div>
