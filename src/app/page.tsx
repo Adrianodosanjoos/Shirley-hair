2190ee56fb50a6595d591755ad60b40a3a01a012
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-03-13 13:20:18 -0300

    .

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,7 @@
 import { Hero } from "./_components/hero";
 import { About } from "./_components/about";
 import { Map } from "./_components/map";
-import { Services } from "./_components/feminino";
+import { Infantil, Services } from "./_components/feminino";
 
 export default function Home() {
   return(
@@ -9,6 +9,7 @@ export default function Home() {
       <Hero/>
       <About/>
       <Services/>
+      <Infantil/>
       <Map/>
     </main>
   )
