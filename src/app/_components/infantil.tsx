d2c672b00e6ed5ff386c1b5fc0ecbe9e9191ac33
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-03-14 13:07:43 -0300

    .

--- src/app/_components/infantil.tsx
+++ src/app/_components/infantil.tsx
@@ -1,14 +1,14 @@
 'use client'
 
 import useEmblaCarousel from 'embla-carousel-react'
-import { ChevronLeft, ChevronRight, Clock, Scissors} from 'lucide-react'
+import { ChevronLeft, ChevronRight, Clock, Scissors } from 'lucide-react'
 import { WhatsappLogo } from "@phosphor-icons/react"
 import trança1 from '../../../public/trança infantil.jpg'
 import trança2 from '../../../public/trança infatil 2.jpg'
 import trança3 from '../../../public/trança infatil3.jpg'
 import trança4 from '../../../public/trança infantil4.jpg'
 import trança5 from '../../../public/trança infantil5.jpg'
-import trança6 from '../../../public/trança femina 6.jpg'
+
 
 
 
@@ -18,9 +18,9 @@ const services = [
         image: trança1,
         duration: "4h",
         price: 'Privado',
-        icon: <Scissors/>,
-        linkText:'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
-       
+        icon: <Scissors />,
+        linkText: 'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
+
     },
 
     {
@@ -28,9 +28,9 @@ const services = [
         image: trança2,
         duration: "2h",
         price: 'Privado',
-        icon: <Scissors/>,
-        linkText:'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
-       
+        icon: <Scissors />,
+        linkText: 'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
+
     },
 
     {
@@ -38,9 +38,9 @@ const services = [
         image: trança3,
         duration: "2h",
         price: 'Privado',
-        icon: <Scissors/>,
-        linkText:'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
-       
+        icon: <Scissors />,
+        linkText: 'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
+
     },
 
     {
@@ -48,9 +48,9 @@ const services = [
         image: trança4,
         duration: "1h a 1h30",
         price: 'Privado',
-        icon: <Scissors/>,
-        linkText:'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
-       
+        icon: <Scissors />,
+        linkText: 'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
+
     },
 
     {
@@ -58,20 +58,11 @@ const services = [
         image: trança5,
         duration: "1h a 1h30",
         price: 'Privado',
-        icon: <Scissors/>,
-        linkText:'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
-       
+        icon: <Scissors />,
+        linkText: 'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
+
     },
 
-    {
-        title: "2",
-        image: trança6,
-        duration: "1h a 1h30",
-        price: 'Privado',
-        icon: <Scissors/>,
-        linkText:'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
-       
-    },
 
 ]
 
@@ -83,93 +74,93 @@ const services = [
 
 export function Infantil() {
 
-       const [emblaRef, emblaApi] = useEmblaCarousel({
-        loop:false,
-        align:'start',
+    const [emblaRef, emblaApi] = useEmblaCarousel({
+        loop: false,
+        align: 'start',
         slidesToScroll: 1,
         breakpoints: {
             '(min-width:768px)': { slidesToScroll: 3 }
         }
-       })
+    })
 
 
-        function scrollPrev(){
-            emblaApi?.scrollPrev();
-        }
+    function scrollPrev() {
+        emblaApi?.scrollPrev();
+    }
 
-        function scrollNext(){
-            emblaApi?.scrollNext();
-        }
+    function scrollNext() {
+        emblaApi?.scrollNext();
+    }
 
 
     return (
-     <section className="bg-black py-16">
-        <div className="container mx-auto px-4">
-            
-            <div className="relative">
+        <section className="bg-black py-16">
+            <div className="container mx-auto px-4">
 
-                     <h3 className="font-serif text-3xl text-white mb-12 flex align-text-center justify-center">Tranças Infantil</h3>
+                <div className="relative">
 
-                <div className='overflow-hidden' ref={emblaRef}>
-                    <div className='flex'>
-                        {services.map((item, index) => (
-                               <div key={index} className=' flex-[0_0_100%] min-w-0 md:flex-[0_0_calc(100%/3)] px-3'>
-                                   <article className='bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full 
+                    <h3 className="font-serif text-3xl text-white mb-12 flex align-text-center justify-center">Tranças Infantil</h3>
+
+                    <div className='overflow-hidden' ref={emblaRef}>
+                        <div className='flex'>
+                            {services.map((item, index) => (
+                                <div key={index} className=' flex-[0_0_100%] min-w-0 md:flex-[0_0_calc(100%/3)] px-3'>
+                                    <article className='bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full 
                                    flex flex-col '>
-                                    <div className='flex-1 items-start justify-between'>
+                                        <div className='flex-1 items-start justify-between'>
 
-                                        <div className='flex gap-3'>
-                                            <span className='text-3xl'>{item.icon}</span>
-                                            <div>
-                                                <h3 className='font-bold text-xl my-1'>{item.title}</h3>
-                                                <img className='select-none' src={item.image.src}  alt={item.title} />
-                                                
+                                            <div className='flex gap-3'>
+                                                <span className='text-3xl'>{item.icon}</span>
+                                                <div>
+                                                    <h3 className='font-bold text-xl my-1'>{item.title}</h3>
+                                                    <img className='select-none' src={item.image.src} alt={item.title} />
+
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
 
-                                    <div className='border-t border-gray-700 pt-4 flex items-center justify-between'>
-                                        <div className='flex items-center gap-2 text-sm'>
-                                            <Clock className='w-4 h-4'/>
-                                            <span>{item.duration}</span>
+                                        <div className='border-t border-gray-700 pt-4 flex items-center justify-between'>
+                                            <div className='flex items-center gap-2 text-sm'>
+                                                <Clock className='w-4 h-4' />
+                                                <span>{item.duration}</span>
                                             </div>
 
                                             <a className='flex items-center justify-center gap-2 
                                             hover:bg-green-500 px-4 py-1 rounded-md duration-300 hover:text-[#1e293b]'
-                                            href="#" target="_blank">
-                                           <WhatsappLogo className="w-4 h-5"/>  Entrar em contato
+                                                href="#" target="_blank">
+                                                <WhatsappLogo className="w-4 h-5" />  Entrar em contato
                                             </a>
-                                    </div>
-                                          
-                                   </article>
-                               </div>
-                        ))}
+                                        </div>
 
+                                    </article>
+                                </div>
+                            ))}
+
+                        </div>
                     </div>
+
+                    <button className='bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute left-3 
+                   -translate-y-1/2 -translate-x-1/2 top-1/2 z-10 cursor-pointer'
+                        onClick={scrollPrev}
+                    >
+                        <ChevronLeft
+                            className='w-6 h-6 text-black '
+                        />
+                    </button>
+
+                    <button className='bg-white  flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute -right-6 
+                   -translate-y-1/2 -translate-x-1/2 top-1/2 z-10 cursor-pointer'
+                        onClick={scrollNext}
+                    >
+                        <ChevronRight
+                            className='w-6 h-6 text-black'
+                        />
+                    </button>
+
                 </div>
-
-                   <button className='bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute left-3 
-                   -translate-y-1/2 -translate-x-1/2 top-1/2 z-10 cursor-pointer'
-                    onClick={scrollPrev}
-                    >
-                    <ChevronLeft 
-                    className='w-6 h-6 text-black '
-                    />
-                   </button>
-
-                   <button className='bg-white  flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute -right-6 
-                   -translate-y-1/2 -translate-x-1/2 top-1/2 z-10 cursor-pointer'
-                    onClick={scrollNext}
-                    >
-                    <ChevronRight 
-                    className='w-6 h-6 text-black'
-                    />
-                   </button>
-
             </div>
-        </div>
-        
+
         </section>
     )
- 
- }
+
+}
--- src/app/_components/maculina.tsx
+++ src/app/_components/maculina.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import useEmblaCarousel from 'embla-carousel-react'
-import { ChevronLeft, ChevronRight, Clock, Scissors} from 'lucide-react'
+import { ChevronLeft, ChevronRight, Clock, Scissors } from 'lucide-react'
 import { Image, WhatsappLogo } from "@phosphor-icons/react"
 import { InstagramLogo } from "@phosphor-icons/react"
 import masculina1 from '../../../public/trança masculina.jpg'
@@ -17,9 +17,9 @@ const services = [
         image: masculina1,
         duration: "4h a 5h",
         price: 'Privado',
-        icon: <Scissors/>,
-        linkText:'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
-       
+        icon: <Scissors />,
+        linkText: 'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
+
     },
 
     {
@@ -27,9 +27,9 @@ const services = [
         image: masculina2,
         duration: "4h a 5h",
         price: 'Privado',
-        icon: <Scissors/>,
-        linkText:'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
-       
+        icon: <Scissors />,
+        linkText: 'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
+
     },
 
     {
@@ -37,20 +37,20 @@ const services = [
         image: masculina3,
         duration: "1h30 a 2",
         price: 'Privado',
-        icon: <Scissors/>,
-        linkText:'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
-       
+        icon: <Scissors />,
+        linkText: 'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
+
     },
 
-    
+
     {
         title: "Nagô masculina",
         image: masculina4,
         duration: "1h30 a 2",
         price: 'Privado',
-        icon: <Scissors/>,
-        linkText:'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
-       
+        icon: <Scissors />,
+        linkText: 'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
+
     },
 
     {
@@ -58,106 +58,106 @@ const services = [
         image: masculina4,
         duration: "1h30 a 2",
         price: 'Privado',
-        icon: <Scissors/>,
-        linkText:'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
-       
+        icon: <Scissors />,
+        linkText: 'Olá, vi no site sobre seu trabalho gostaria de mais informações.'
+
     },
 
-   
+
 ]
 
 
 export function Masculina() {
 
-       const [emblaRef, emblaApi] = useEmblaCarousel({
-        loop:false,
-        align:'start',
+    const [emblaRef, emblaApi] = useEmblaCarousel({
+        loop: false,
+        align: 'start',
         slidesToScroll: 1,
         breakpoints: {
             '(min-width:768px)': { slidesToScroll: 3 }
         }
-       })
+    })
 
 
-        function scrollPrev(){
-            emblaApi?.scrollPrev();
-        }
+    function scrollPrev() {
+        emblaApi?.scrollPrev();
+    }
 
-        function scrollNext(){
-            emblaApi?.scrollNext();
-        }
+    function scrollNext() {
+        emblaApi?.scrollNext();
+    }
 
 
     return (
-     <section className="bg-black py-16">
-        <div className="container mx-auto px-4">
-          
-            
-            <div className="relative">
+        <section className="bg-black py-16">
+            <div className="container mx-auto px-4">
 
-                     <h3 className="font-serif text-3xl text-white mb-12 flex align-text-center justify-center">Tranças Masculinas</h3>
 
-                <div className='overflow-hidden' ref={emblaRef}>
-                    <div className='flex'>
-                        {services.map((item, index) => (
-                               <div key={index} className=' flex-[0_0_100%] min-w-0 md:flex-[0_0_calc(100%/3)] px-3'>
-                                   <article className='bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full 
+                <div className="relative">
+
+                    <h3 className="font-serif text-3xl text-white mb-12 flex align-text-center justify-center">Tranças Masculinas</h3>
+
+                    <div className='overflow-hidden' ref={emblaRef}>
+                        <div className='flex'>
+                            {services.map((item, index) => (
+                                <div key={index} className=' flex-[0_0_100%] min-w-0 md:flex-[0_0_calc(100%/3)] px-3'>
+                                    <article className='bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 h-full 
                                    flex flex-col '>
-                                    <div className='flex-1 items-start justify-between'>
+                                        <div className='flex-1 items-start justify-between'>
 
-                                        <div className='flex gap-3'>
-                                            <span className='text-3xl'>{item.icon}</span>
-                                            <div>
-                                                <h3 className='font-bold text-xl my-1'>{item.title}</h3>
-                                                <img className='select-none' src={item.image.src}  alt={item.title} />
-                                                
+                                            <div className='flex gap-3'>
+                                                <span className='text-3xl'>{item.icon}</span>
+                                                <div>
+                                                    <h3 className='font-bold text-xl my-1'>{item.title}</h3>
+                                                    <img className='select-none' src={item.image.src} alt={item.title} />
+
+                                                </div>
                                             </div>
                                         </div>
-                                    </div>
 
-                                    <div className='border-t border-gray-700 pt-4 flex items-center justify-between'>
-                                        <div className='flex items-center gap-2 text-sm'>
-                                            <Clock className='w-4 h-4'/>
-                                            <span>{item.duration}</span>
+                                        <div className='border-t border-gray-700 pt-4 flex items-center justify-between'>
+                                            <div className='flex items-center gap-2 text-sm'>
+                                                <Clock className='w-4 h-4' />
+                                                <span>{item.duration}</span>
                                             </div>
 
                                             <a className='flex items-center justify-center gap-2 
                                             hover:bg-green-500 px-4 py-1 rounded-md duration-300 hover:text-[#1e293b]'
-                                            href="#" target="_blank">
-                                           <WhatsappLogo className="w-4 h-5"/>  Entrar em contato
+                                                href="#" target="_blank">
+                                                <WhatsappLogo className="w-4 h-5" />  Entrar em contato
                                             </a>
-                                    </div>
-                                          
-                                   </article>
-                               </div>
-                        ))}
+                                        </div>
 
+                                    </article>
+                                </div>
+                            ))}
+
+                        </div>
                     </div>
+
+                    <button className='bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute left-3 
+                   -translate-y-1/2 -translate-x-1/2 top-1/2 z-10 cursor-pointer'
+                        onClick={scrollPrev}
+                    >
+                        <ChevronLeft
+                            className='w-6 h-6 text-black '
+                        />
+                    </button>
+
+                    <button className='bg-white  flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute -right-6 
+                   -translate-y-1/2 -translate-x-1/2 top-1/2 z-10 cursor-pointer'
+                        onClick={scrollNext}
+                    >
+                        <ChevronRight
+                            className='w-6 h-6 text-black'
+                        />
+                    </button>
+
                 </div>
-
-                   <button className='bg-white flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute left-3 
-                   -translate-y-1/2 -translate-x-1/2 top-1/2 z-10 cursor-pointer'
-                    onClick={scrollPrev}
-                    >
-                    <ChevronLeft 
-                    className='w-6 h-6 text-black '
-                    />
-                   </button>
-
-                   <button className='bg-white  flex items-center justify-center rounded-full shadow-lg w-10 h-10 absolute -right-6 
-                   -translate-y-1/2 -translate-x-1/2 top-1/2 z-10 cursor-pointer'
-                    onClick={scrollNext}
-                    >
-                    <ChevronRight 
-                    className='w-6 h-6 text-black'
-                    />
-                   </button>
-
             </div>
-        </div>
-        
+
         </section>
     )
- 
- }
+
+}
 
