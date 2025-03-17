ddc1dd712b82c62b3e02e30427d076bc070ab626
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-03-17 13:53:35 -0300

    .

--- src/app/_components/footer.tsx
+++ src/app/_components/footer.tsx
@@ -1,46 +1,48 @@
+'use client'
 
+import React from 'react'
+import useEmblaCarousel from 'embla-carousel-react'
 import Image from 'next/image'
 import esponjamagic from '../../../public/esponja magic fundo branco produto5.jpg'
 import zhanghair from '../../../public/Logo-Zhang-Hair produto 1.png'
 import dsbeaty from '../../../public/ds beaty produto 4.png'
 import duchapeu from '../../../public/du chapeu hair produto2.png'
 import theclass from '../../../public/the class produto 3.png'
-import imperioblack from '../../../public/ Império Black produto 6.png'
-
+import imperioblack from '../../../public/ Império Black produto 6.png'
 
 const brands = [
-    {name:"Império Black", logo: imperioblack },
-    {name:"marca do produto", logo: theclass },
-    {name:"Du Chapéu", logo: duchapeu },
-    {name:"DS Beaty", logo: dsbeaty },
-    {name:"Zhang Hair", logo: zhanghair },
-    {name:"Esponja Magic", logo: esponjamagic },
+    { name: "Império Black", logo: imperioblack },
+    { name: "The Class", logo: theclass },
+    { name: "Du Chapéu", logo: duchapeu },
+    { name: "DS Beaty", logo: dsbeaty },
+    { name: "Zhang Hair", logo: zhanghair },
+    { name: "Esponja Magic", logo: esponjamagic },
 ]
 
 export function Footer() {
+    const [emblaRef] = useEmblaCarousel({ loop: true, align: 'center' })
+
     return (
         <section className='bg-[#373739] py-16 text-white'>
-                   <div className='container mx-auto px-4'>
-
-                        <div className='border-b  border-black pb-8'>
-                            <h4 className='text-3xl font-semibold mb-8 text-center'>Marcas que trabalhamos</h4>
-
-                                <div  className='flex gap-2 md:items-center md:content-center'>
-                                    {brands.map((item, index) => (
-                                        <div key={index}>
+            <div className='container mx-auto px-4'>
+                <div className='border-b border-black pb-8'>
+                    <h4 className='text-3xl font-semibold mb-8 text-center'>Marcas que trabalhamos</h4>
+                    <div ref={emblaRef} className='overflow-hidden'>
+                        <div className='flex'>
+                            {brands.map((item, index) => (
+                                <div key={index} className='flex-none w-[150px] mx-2'>
                                     <Image
-                                     src={item.logo}
-                                     alt={item.name}
-                                     height={50}
-                                     className="object-contain"
-                                     />
-                                     </div>
-                                    ))}
+                                        src={item.logo}
+                                        alt={item.name}
+                                        height={50}
+                                        className="object-contain"
+                                    />
                                 </div>
-
+                            ))}
                         </div>
-
-                   </div>
+                    </div>
+                </div>
+            </div>
         </section>
     )
 }
