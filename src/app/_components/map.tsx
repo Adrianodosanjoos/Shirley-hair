8b809c4df6f020a4d4e1605e612accf6e00fe119
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-03-19 10:08:40 -0300

    .

--- src/app/_components/map.tsx
+++ src/app/_components/map.tsx
@@ -16,11 +16,20 @@ export  function Map() {
      
   };
 
+    // Função para redirecionar ao Google Maps
+    const handleMapClick = () => {
+      const googleMapsUrl = `https://www.google.com/maps?q=${center.lat},${center.lng}`;
+      window.open(googleMapsUrl, '_blank'); // Abre o link em uma nova aba
+    };
+  
+
   return (
     <div>
       <h1>Mapa do Google</h1>
       <LoadScript googleMapsApiKey="AIzaSyCuws2A5d9BaGKAORsvzMH_lCx7UzanR4Y">
-        <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={15}>
+        <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={15}
+           onClick={handleMapClick} // Adicionado evento de clique no mapa
+>
           <Marker position={center} />
         </GoogleMap>
       </LoadScript>
