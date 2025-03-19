ee320186578296bea94f8ade8726467c88bbcee7
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-03-19 16:42:49 -0300

    .

--- src/app/_components/low.tsx
+++ src/app/_components/low.tsx
@@ -7,10 +7,10 @@ export function Low() {
      <div>
         
 
-            <div className="bg-black text-white font-serif py-16">
+            <div className="bg-black text-white font-serif py-16 space-y-6">
 
             
-                <h2 className="text-3xl lg:text-4xl font-serif flex align-text-center justify-center space-y-6">Contatos</h2>
+                <h2 className="text-3xl lg:text-4xl font-serif flex align-text-center justify-center">Contatos</h2>
 
                 <div className=" flex align-text-center justify-center gap-2 ">
                 <a href="https://www.instagram.com/shirleibraids/" target="_blank"
@@ -30,7 +30,7 @@ export function Low() {
                     </div>
 
 
-                <p className=" lg:text-2xl font-bold flex align-text-center justify-center">R: José Cardoso Xavier, 452 Jd. Cacique-Suzano | SP</p>
+                <p className="text-10px lg:text-2xl font-bold flex align-text-center justify-center">R: José Cardoso Xavier, 452 Jd. Cacique-Suzano | SP</p>
 
             </div>
 
