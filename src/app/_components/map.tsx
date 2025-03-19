65ffd7a9c900bffe82c54e814f35f6560ade6f80
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-03-19 13:31:25 -0300

    .

--- src/app/_components/map.tsx
+++ src/app/_components/map.tsx
@@ -1,38 +1,46 @@
-'use client'
+
+'use client';
 
 import { GoogleMap, LoadScript, Marker } from '@react-google-maps/api';
 
-export  function Map() {
+export function Map() {
   const mapContainerStyle = {
     width: '100%',
     height: '400px',
   };
 
-  // Coordenadas aproximadas da localização do link
+  // Coordenadas aproximadas da localização
   const center = {
-    lat: -23.557766394871813, // Latitude de exemplo (ajuste conforme necessário)
-    lng: -46.291849641248 // Longitude de exemplo (ajuste conforme necessário)
-
-     
+    lat: -23.557766394871813, // Latitude de exemplo
+    lng: -46.291849621448, // Longitude de exemplo
   };
 
-    // Função para redirecionar ao Google Maps
-    const handleMapClick = () => {
-      const googleMapsUrl = `https://www.google.com/maps?q=${center.lat},${center.lng}`;
-      window.open(googleMapsUrl, '_blank'); // Abre o link em uma nova aba
-    };
-  
+  // Função para redirecionar ao Google Maps
+  const handleMapClick = () => {
+    const googleMapsUrl = `https://www.google.com/maps?q=${center.lat},${center.lng}`;
+      window.open(googleMapsUrl, '_blank', 'noopener,noreferrer');
+  };
 
   return (
-    <div>
-      <h1 className='flex items-center text-3xl lg:text-4xl font-serif'>Onde estamos localizado</h1>
-      <LoadScript googleMapsApiKey="AIzaSyDekM5cIp-ZYlp37KCgciK6U8KWHmodovQ">
-        <GoogleMap mapContainerStyle={mapContainerStyle} center={center} zoom={15}
-           onClick={handleMapClick} // Adicionado evento de clique no mapa
->
+    <div className="bg-black py-16 text-white">
+      <h1 className="flex align-text-center text-xl lg:text-4xl font-serif mb-2">
+        Onde estamos localizados
+      </h1>
+
+      <LoadScript 
+        googleMapsApiKey="SUA_CHAVE_AQUI" 
+        libraries={["places"]}
+      >
+        <GoogleMap 
+          mapContainerStyle={mapContainerStyle} 
+          center={center} 
+          zoom={15} 
+          onClick={handleMapClick}
+        >
           <Marker position={center} />
         </GoogleMap>
       </LoadScript>
-    </div>
-  );
+    </div>
+  );
 }
+
