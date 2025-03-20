29f1463a35451cdd6ea71adec61bb32521770287
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-03-20 14:15:41 -0300

    .

--- src/app/_components/testimonials.tsx
+++ src/app/_components/testimonials.tsx
@@ -79,7 +79,7 @@ export function Testimonials() {
                     <div className='overflow-hidden' ref={emblaRef}>
                         <div className='flex'>
                             {testimonials.map((item, index) => (
-                                <div key={index} className=' flex-[0_0_100%] min-w-0 px-3'>
+                                <div key={index} className=' md:flex-[0_0_calc(100%)] px-3'>
                                     <article className='bg-[#1e293b] text-white rounded-2xl p-6 space-y-4 
                                    flex flex-col '>
 
@@ -99,7 +99,7 @@ export function Testimonials() {
                                                     src={item.image}
                                                     alt='foto do depoimento'
                                                     quality={100}
-                                                    className='select-none relative h-94'
+                                                    className='select-none container h-94'
                                                 />
                                             </div>
 
