b0e1f059b728f9f758db37ab54efe155165f6002
adrianodosanjoos <adriano.luizysa@gmail.com>
2025-03-20 12:23:52 -0300

    .

--- src/app/_components/testimonials.tsx
+++ src/app/_components/testimonials.tsx
@@ -72,7 +72,7 @@ export function Testimonials() {
             <div className="container mx-auto px-2">
 
 
-                <div className="relative max-w-2xl mx-auto">
+                <div className="relative max-w-1xl mx-auto">
 
                     <h3 className="font-serif text-4xl text-white mb-12 flex align-text-center justify-center">Depoimentos</h3>
 
